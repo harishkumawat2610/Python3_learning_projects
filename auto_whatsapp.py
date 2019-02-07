593fd7b451bb51eba9ecd168cfc153de664424be
HARISH KUMAWAT <39532868+harishkumawat2610@users.noreply.github.com>
2019-02-07 17:03:31 +0530

    project 3 auto_what'sapp

---
+++ auto_whatsapp.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get('https://web.whatsapp.com')
+input('Enter anything after scanning QR code..typing ok  ')
+
+name = input('Enter the name of user or group : ')
+
+count = int(input('Enter the count : '))
+
+
+
+user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+user.click()
+
+
+
+
+if count >=2:
+	msg = input('Enter your message : ')
+	for i in range(count):
+		msg_box = driver.find_element_by_class_name('_2S1VP')
+		msg_box.send_keys(msg)
+		button = driver.find_element_by_class_name('_35EW6')
+		button.click()
+elif count == 1:
+	while True:
+		msg = input('Enter your message : ')
+		msg_box = driver.find_element_by_class_name('_2S1VP')
+		msg_box.send_keys(msg)
+		button = driver.find_element_by_class_name('_35EW6')
+		button.click()
+		if msg == 'stop':
+			break
